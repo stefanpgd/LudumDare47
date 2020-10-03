798f86650582c79c9148d6ec0481d515a3b498c7
Zoran <zster1204@gmail.com>
2020-10-03 14:02:20 +0200

    Player / Enemy
    Basic 2D Player Movement
    AI Movement
    AI Spawner
    Player Health

---
+++ Assets/Script/PlayerHealth.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float Health;
+    public TextMeshProUGUI HealthText;
+
+    private float StartInvulnerableTimer;
+    [SerializeField] private float InvulnerableTimer;
+
+    private bool CanTakeDamage;
+
+    void Start()
+    {
+        StartInvulnerableTimer = InvulnerableTimer;
+        HealthText.text = "" + Health;
+    }
+
+    void Update()
+    {
+        if (InvulnerableTimer > 0)
+        {
+            InvulnerableTimer -= 1 * Time.deltaTime;
+        }
+        else
+        {
+            CanTakeDamage = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy") && CanTakeDamage)
+        {
+            InvulnerableTimer = StartInvulnerableTimer;
+            CanTakeDamage = false;
+            Health--;
+            HealthText.text = "" + Health;
+        }
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D RigidBody;
+
+    [SerializeField] private float MoveSpeed = 5.0f;
+    [SerializeField] private float DiagonalMoveSpeed = 0.7f;
+
+    private float Horizontal;
+    private float Vertical;
+    
+    private void Start()
+    {
+        RigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Horizontal = Input.GetAxisRaw("Horizontal");
+        Vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (Horizontal != 0 && Vertical != 0)
+        {
+            Horizontal *= DiagonalMoveSpeed;
+            Vertical *= DiagonalMoveSpeed;
+        }
+
+        RigidBody.velocity = new Vector2(Horizontal * MoveSpeed, Vertical * MoveSpeed);
+    }
+}
