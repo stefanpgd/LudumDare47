831de951e1eaaade0578697d7def8036759e9da5
Stefan Pijnacker <60875649+talespin-stefan@users.noreply.github.com>
2020-10-03 20:51:06 +0200

    Linked weapon to player

--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -4,38 +4,43 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    private Vector3 mousePosition;
-    public Transform playerArm;
-    public GameObject bullet;
-    public GameObject crosshair;
-    public Transform firePoint;
-    public float bulletSpeed = 5f;
+    [SerializeField] private GameObject crosshair;
+    [SerializeField] private GameObject playerArm;
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private GameObject projectileStart;
+    [SerializeField] private float bulletSpeed = 20f;
 
-    private void Start()
+    private Vector3 target;
+
+    // Use this for initialization
+    void Start()
     {
         Cursor.visible = false;
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
-        crosshair.transform.position = new Vector3(mousePosition.x, mousePosition.y);
-        Vector3 difference = mousePosition - playerArm.position;
+        target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        crosshair.transform.position = new Vector2(target.x, target.y);
+
+        Vector3 difference = target - playerArm.transform.position;
         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        playerArm.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+        playerArm.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
 
         if (Input.GetMouseButtonDown(0))
         {
             float distance = difference.magnitude;
             Vector2 direction = difference / distance;
             direction.Normalize();
-            Fire(direction, rotationZ);
+            fireBullet(direction, rotationZ);
         }
     }
-    private void Fire(Vector2 dir, float RotationZ)
+
+    void fireBullet(Vector2 direction, float rotationZ)
     {
-        GameObject b = Instantiate(bullet, firePoint.position, Quaternion.identity);
-        b.transform.rotation = Quaternion.Euler(0f, 0f, RotationZ);
-        b.GetComponent<Rigidbody2D>().velocity = dir * bulletSpeed;
+        GameObject b = Instantiate(projectilePrefab, projectileStart.transform.position, Quaternion.identity);
+        b.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+        b.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
     }
 }
