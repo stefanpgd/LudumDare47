35a80c6f7a3de8c0119787866cf100d9a0d9dd4f
Stefan Pijnacker <stefanpijnacker@gmail.com>
2020-10-03 14:52:45 +0200

    Finished resourceManager together with UIDisplay

--- Assets/Script/ResourceType.cs
+++ Assets/Script/ResourceType.cs
@@ -1,18 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ResourceType : MonoBehaviour
+﻿public enum ResourceType 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Gold,
+    Kills
 }
