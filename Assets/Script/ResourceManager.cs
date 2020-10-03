93fb7eb35698d6160abf595b6d6426275a63bffe
Stefan Pijnacker <stefanpijnacker@gmail.com>
2020-10-03 14:13:10 +0200

    Setup scripts

---
+++ Assets/Script/ResourceManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#pragma warning disable 649
+
+public class ResourceManager : MonoBehaviour
+{
+    public static ResourceManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+
+}
