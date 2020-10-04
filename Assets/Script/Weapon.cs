f214cf7f0f98bd97f087856a99b967c53690e1a0
Zoran <zster1204@gmail.com>
2020-10-04 14:54:14 +0200

    Player prefab / Arrow prefab
    Weapon scripts shoot delay

--- Assets/Script/Weapon.cs
+++ Assets/Script/Weapon.cs
@@ -7,6 +7,8 @@ public class Weapon : MonoBehaviour
     [SerializeField] private GameObject playerArm;
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private GameObject projectileStart;
+    [SerializeField] private float ShootDelay = 0.4f;
+    private float StartShootDelay;
     [SerializeField] private float bulletSpeed = 20f;
 
     private Vector3 target;
@@ -14,10 +16,14 @@ public class Weapon : MonoBehaviour
     void Start()
     {
         Cursor.visible = false;
+
+        StartShootDelay = ShootDelay;
     }
 
     void Update()
     {
+        ShootDelay -= 1 * Time.deltaTime;
+
         target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
         crosshair.transform.position = new Vector2(target.x, target.y);
 
@@ -25,12 +31,14 @@ public class Weapon : MonoBehaviour
         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         playerArm.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0) && ShootDelay <= 0)
         {
             float distance = difference.magnitude;
             Vector2 direction = difference / distance;
             direction.Normalize();
             fireBullet(direction, rotationZ);
+
+            ShootDelay = StartShootDelay;
         }
     }
 
