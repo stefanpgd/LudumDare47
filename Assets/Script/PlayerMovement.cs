63a30aa2cee13e4dca67536f2e92cba4ad1850ee
Zoran <zster1204@gmail.com>
2020-10-04 22:21:38 +0200

    player stuff

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D RigidBody;
+    Animator anim;
 
     [SerializeField] private float MoveSpeed = 5.0f;
     [SerializeField] private float DiagonalMoveSpeed = 0.7f;
+
+    private bool IsMoving;
 
     private float Horizontal;
     private float Vertical;
@@ -15,6 +18,7 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         RigidBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -25,6 +29,13 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (RigidBody.velocity.magnitude > 0 || RigidBody.velocity.magnitude < 0)
+        {
+            anim.SetBool("IsMoving", true);
+        }
+        else
+            anim.SetBool("IsMoving", false);
+
         if (Horizontal != 0 && Vertical != 0)
         {
             Horizontal *= DiagonalMoveSpeed;
